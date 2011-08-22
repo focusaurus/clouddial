13918e2d2ecf0f316b1f97fcc53397fbafea5233
Peter Lyons <pete@peterlyons.com>
2011-08-21 20:44:41 -0600

    Decoupled results from command more cleanly

--- cookbooks/clouddial/recipes/default.rb
+++ cookbooks/clouddial/recipes/default.rb
@@ -1,5 +1,8 @@
 conf = data_bag_item('clouddial_conf', 'conf')['value']
 
+#Note it might be clearer to install packages in order regardless of type
+#but for now this is acceptable
+
 #Install any APT packages
 apt_packages = conf['packages'].select {|p| p['type'] == 'APT'}
 apt_names = apt_packages.map {|p| p['name']}
@@ -13,26 +16,22 @@ end
 tgz_packages = conf['packages'].select {|p| p['type'] == 'TGZ'}
 tgz_packages.each do |pkg|
   bash "Install S3 tarball #{pkg['name']}" do
-    flags '-e'
     #TODO stricter path sanitization. Find the ruby equivalent of python's
     #commands.mkarg
     path = "'/tmp/#{pkg['name']}.tar.gz'"
     tmp_code = <<-EOF
+    #TODO AWS S3 authentication token integration
     curl -s --output #{path} '#{pkg['URL']}'
     #Only delete the package if it successfully extracts
     tar xvzf #{path} -C / && rm -f #{path}
     EOF
-    out_file = File.open('/tmp/tmp_code.sh', 'w')
-    out_file.write tmp_code
-    out_file.close
-    #TODO authentication token integration
     code tmp_code
   end
 end
 
 post_command = conf['post_command']
 if post_command
-  execute 'post_command' do
-    command post_command
+  bash 'post_command' do
+    code "mkdir /tmp/rg_results\n" + post_command
   end
 end
